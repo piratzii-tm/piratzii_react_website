87c7c4a050450694516a86d594bd4ee662844649
poenaruiulian <poenaruiulian2003@gmail.com>
2023-06-02 12:55:16 +0300

    GitHub pages

--- website/src/index.js
+++ website/src/index.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import {HashRouter} from "react-router-dom";
 
 import App from './App';
 
@@ -10,9 +9,7 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-    <StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
-    </StrictMode>
+        </BrowserRouter>
 );
