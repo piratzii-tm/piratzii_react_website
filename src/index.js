6caecf34fef2476fdf9619de901e1a833d55d7dd
poenaruiulian <poenaruiulian2003@gmail.com>
2023-06-03 15:55:58 +0300

    Fix basename for gh pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = createRoot(rootElement);
 
 root.render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </StrictMode>
